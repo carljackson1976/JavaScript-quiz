95289addd184ebe537c88eaa63bcfeb9a1743bd4
Carl Crawley <carljackson1976@gmail.com>
2016-06-17 11:58:57 -0500

    final quiz

--- quiz.js
+++ quiz.js
@@ -1,61 +1,48 @@
-var height = document.getElementById('height');
-	var character = document.getElementById('character');
-	var button = document.getElementById('button');
-	var branch = [];
+//links these variables to HTML for button, and input fields
+ var character= document.getElementById("character");
+ var height=document.getElementById("height");
+ var button= document.getElementById("button");
 
-
-	button.addEventListener('click', buttonHandler);
-	height.addEventListener('keypress', keypressHandler);
-	character.addEventListener('keypress', keypressHandler);
-
-
-	function buttonHandler() {
-		if (validate()) {
-			makeTree(height.value, character.value);
-		}
+//handles button click, or enter press release to make tree
+ button.addEventListener("click", function(){
+	tree(treeObject);
+ });
+ height.addEventListener("keyup", function(){
+	treeObject.height = parseInt(height.value)+1 ;
+	if(event.code==="Enter"){
+		tree(treeObject);
 	}
-
-
-	function keypressHandler(event) {
-		if (event.keyCode === 7) {
-			if (validate()) {
-				makeTree(height.value, character.value);
-			}
-		}
+ });
+ character.addEventListener("keyup", function(){
+	treeObject.character = character.value.charAt(0);
+	if(event.code==="Enter"){
+		tree(treeObject);
 	}
+ });
+
+//object that holds the height and character that gets passed into tree function
+ var treeObject = {
+	height: "",
+	character: ""
+ };
 
 
-	function validate() {
-		var valid = false;
-		if (!height.value || !character.value) {
-			alert('please enter both a height and character and then hit enter or push button');
-		} else {
-			valid = true;
-		}
-		return valid;
+ function tree(treeObj){
+	//alerts user if no values are input
+	if(treeObj.height === "" || treeObj.character === ""){
+		alert("please enter both a height and character and then hit enter or push button");
 	}
+	//for loops (increments for tree formation)
+	else{
+	var print = "";
+		for(var i=0; i<treeObj.height; i++){
+			
+			for(var x=i; x<=treeObj.height; x++){
+			print += " ";}
 
-
-	function makeTree(height, character) {
-		for (var x = 0; x < height; x++) {
-			makeBranch(x + 1, height - (x + 1), character);
-		}
-	}
-
-
-	function makeBranch(chars, spaces, character) {
-		for (var x = 0; x < chars; x++) {
-			branch.push(character);
-			if (x > 0) {
-				branch.unshift(character);
-			}
-		}  
-		for (var y = spaces; y > 0; y--) {
-			branch.push(" ");
-			branch.unshift(" ");
-		}
-		branch = branch.join('');
-		console.log(branch);
-		branch=[];
-	}
-
+			for(var y=0; y<i*2-1; y++){
+			print += treeObj.character;}
+	    //'new line' to properly display user input values in a tree-shaped form while printing to console
+		print += "\n";}
+	console.log(print);}	
+ }	
